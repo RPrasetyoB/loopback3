bb1a89cee4a385479b396aa1b2bf7dbc7171386b
RPrasetyoB <rpbasuki.dev@gmail.com>
2024-04-14 14:28:58 +0700

    feat: add template lb init

---
+++ server/server.js
@@ -0,0 +1,39 @@
+// Copyright IBM Corp. 2016,2019. All Rights Reserved.
+// Node module: loopback-workspace
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+'use strict';
+
+const loopback = require('loopback');
+const boot = require('loopback-boot');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = module.exports = loopback();
+
+app.start = function() {
+  return app.listen(function() {
+    app.emit('started');
+    const baseUrl = app.get('url').replace(/\/$/, '');
+    console.log('Web server listening at: %s', baseUrl);
+  });
+};
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.enableAuth();
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+boot(app, __dirname, function(err) {
+  if (err) throw err;
+
+  require('./boot/customer')(app);
+
+  if (require.main === module)
+    app.start();
+});
---
+++ server/utils.js
@@ -0,0 +1,24 @@
+'use strict';
+const jwt = require('jsonwebtoken');
+const jwtKey = process.env.JWT_SECRET;
+module.exports = {
+  getToken: function(req) {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      return null;
+    }
+    const token = authHeader.split(' ')[1];
+    try {
+      const decoded = jwt.verify(token, jwtKey);
+      return decoded;
+    } catch (error) {
+      return null;
+    }
+  },
+  loggedUser: function(decodedToken) {
+    return {
+      userId: decodedToken.userId,
+    };
+  },
+};
